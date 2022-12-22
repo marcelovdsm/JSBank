14ff15b366ecb76629c1620b45ecadd79018e6fc
Marcelo Victor <95694179+marcelovdsm@users.noreply.github.com>
2022-12-22 14:34:28 +0000

    Finalizado a página de login

--- exercicio22-banco-js/App.js
+++ exercicio22-banco-js/App.js
@@ -48,3 +48,13 @@ module.exports = class App {
     Loan.fee = newFeePercentage
   }
 }
+
+const togglePassword = document.getElementById('togglePassword')
+const password = document.querySelector('#togglePassword');
+
+togglePassword.addEventListener('click', function (e) {
+  const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+  password.setAttribute('type', type);
+  this.classList.toggle('fa-eye-slash');
+  console.log("Clicou");
+});
